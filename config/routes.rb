33b260396b58d36f62cc4e337c35d98abb9fa7ca
吉本光輝 <yoyoyo427435@gmail.com>
2018-10-16 19:52:03 +0900

    :sparkles: generate model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :employers
+  devise_for :admins
+  devise_for :workers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
