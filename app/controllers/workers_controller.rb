aba2f745fbecc7874b3303479f418000a678b997
吉本光輝 <yoyoyo427435@gmail.com>
2018-10-22 20:32:29 +0900

    :sparkles: workerのコントローラーを作った

--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -1,3 +1,12 @@
 class WorkersController < ApplicationController
   layout 'workers/application'
+  def index
+    @worker = Worker.find(current_user.id)
+  end
+  def create
+  end
+  def show
+  end
+  def update
+  end
 end
